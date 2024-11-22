cdd0b251c13578566db6f598a4702c91cf50ca2f
roufrubel <rubel.ma2012@gmail.com>
2024-11-22 00:41:40 +0000

    students created successfully

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,19 @@
 import express, { Application, Request, Response } from 'express';
-import cors from "cors";
-const app : Application = express();
+import cors from 'cors';
+import { StudentRoutes } from './app/modules/student/student.route';
+const app: Application = express();
 // const port = 3000;
 
 // parsers
-app.use(express.json())
+app.use(express.json());
 app.use(cors());
 
-app.get('/', (req : Request, res: Response) => {
-  let b;
+// application routes
+app.use('/api/v1/students', StudentRoutes);
+
+app.get('/', (req: Request, res: Response) => {
   const a = 10;
-  res.send(a)
+  res.send(a);
 });
 
 export default app;
-
